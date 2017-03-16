d547b53cdfcae29889541c97787f5dc404128e40
shybbq <shy13331115827@163.com>
2017-03-16 18:01:35 +0800

    admin1

---
+++ yzf-cloud-auth-admin/src/main/java/com/cloud/auth/admin/YzfCloudAuthAdminApplication.java
@@ -0,0 +1,12 @@
+package com.cloud.auth.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YzfCloudAuthAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YzfCloudAuthAdminApplication.class, args);
+	}
+}
