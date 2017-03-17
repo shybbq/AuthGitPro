14e08413567d6337ebb245dec4399921c287503c
shybbq <shy13331115827@163.com>
2017-03-17 17:09:39 +0800

    1

--- yzf-cloud-auth-admin/src/main/java/com/cloud/auth/admin/YzfCloudAuthAdminApplication.java
+++ yzf-cloud-auth-admin/src/main/java/com/cloud/auth/admin/YzfCloudAuthAdminApplication.java
@@ -2,8 +2,10 @@ package com.cloud.auth.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(locations={"classpath:spring/spring.xml"})
 public class YzfCloudAuthAdminApplication {
 
 	public static void main(String[] args) {
