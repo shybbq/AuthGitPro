63471e120ecdf094e92ce4b3d3a543742f15c970
shybbq <shy13331115827@163.com>
2017-03-17 17:21:41 +0800

    这是个完整的注册发现中心

--- yzf-cloud-eureka-server/src/main/java/com/cloud/auth/eureka/YzfCloudEurekaServerApplication.java
+++ yzf-cloud-eureka-server/src/main/java/com/cloud/auth/eureka/YzfCloudEurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.cloud.auth.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class YzfCloudEurekaServerApplication {
 
