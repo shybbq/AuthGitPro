b90082aecb8034bc2396e2b4e2f7c22000013223
shybbq <shy13331115827@163.com>
2017-03-17 18:47:01 +0800

    无法注册启动

--- yzf-cloud-auth-common/src/main/java/com/yzf/cloud/YzfCloudAuthCommonApplication.java
+++ yzf-cloud-auth-common/src/main/java/com/yzf/cloud/YzfCloudAuthCommonApplication.java
@@ -2,9 +2,11 @@ package com.yzf.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 // 系统基础服务抽象 包括 entity dao service 的抽象
 @SpringBootApplication
+@EnableEurekaClient
 public class YzfCloudAuthCommonApplication {
 
 	public static void main(String[] args) {
