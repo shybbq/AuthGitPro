1a2b67783ef53559ef68a208d21a8f793b39c5e6
shybbq <shy13331115827@163.com>
2017-03-17 17:09:55 +0800

    这是个注册发现服务

---
+++ yzf-cloud-eureka-server/src/main/java/com/cloud/auth/eureka/YzfCloudEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.cloud.auth.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YzfCloudEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YzfCloudEurekaServerApplication.class, args);
+	}
+}
